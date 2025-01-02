022f67a37eff2e393327e555977e55803c0444f8
Tyrone Trevorrow <819705+tyrone-sudeium@users.noreply.github.com>
2025-01-02 18:12:31 +0900

    fix: why does %b have its own space??

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn main_daemon() -> anyhow::Result<()> {
 
                 // Add a notification object to the pushover bridge API JSON payload
                 let time = sub.return_time.with_timezone(&Local);
-                let time_str = time.format("%b %e, %Y, %I:%M%p").to_string();
+                let time_str = time.format("%b%e, %Y, %I:%M%p").to_string();
                 let body = if subs_in_group > 1 {
                     format!(
                         "{name} ({char_name} «{tag}») + {num} others returned on {time_str}",
@@ -123,7 +123,7 @@ fn main_daemon() -> anyhow::Result<()> {
                 meta.will_notify = false;
                 let summary = format!("{name} returned", name = sub.name);
                 let time = sub.return_time.with_timezone(&Local);
-                let time_str = time.format("%b %e, %Y, %I:%M%p").to_string();
+                let time_str = time.format("%b%e, %Y, %I:%M%p").to_string();
                 let body = format!(
                     "{name} ({char_name} «{tag}») returned on {time_str}",
                     name = sub.name,
